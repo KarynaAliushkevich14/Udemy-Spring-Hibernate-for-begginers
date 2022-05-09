15f1bdc7710f89e71563842899c79b44c9a7c9d5
KarynaAliushkevich14 <karyna.alyshkevich@gmail.com>
2022-05-09 20:47:29 +0200

    First Spring Project.Usual MyApp.java vs Spring Beans

---
+++ spring-demo-one/src/com/luv2code/springdemo/GolfCoach.java
@@ -0,0 +1,10 @@
+package com.luv2code.springdemo;
+
+public class GolfCoach implements Coach {
+
+    @Override
+    public String getDailyWorkOut(){
+        return "Let's play some golf";
+    }
+
+}
---
+++ spring-demo-one1/src/main/java/com/example/com_luv2code_springdemo/SpringDemoOne1Application.java
@@ -0,0 +1,13 @@
+package com.example.com_luv2code_springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoOne1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoOne1Application.class, args);
+	}
+
+}
