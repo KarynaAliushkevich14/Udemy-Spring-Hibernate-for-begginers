c49134d266c4bd6c7873dd47a903af23eabd2a93
KarynaAliushkevich14 <karyna.alyshkevich@gmail.com>
2022-05-16 11:39:38 +0200

    Add Dependency Injection

--- spring-demo-one/src/com/luv2code/springdemo/GolfCoach.java
+++ spring-demo-one/src/com/luv2code/springdemo/GolfCoach.java
@@ -1,6 +1,13 @@
 package com.luv2code.springdemo;
 
 public class GolfCoach implements Coach {
+    //field of "interface"
+    FortuneService fortuneService;
+
+    //class constructor
+    GolfCoach(FortuneService theFortuneService){
+      fortuneService= theFortuneService;
+    }
 
     @Override
     public String getDailyWorkOut(){
