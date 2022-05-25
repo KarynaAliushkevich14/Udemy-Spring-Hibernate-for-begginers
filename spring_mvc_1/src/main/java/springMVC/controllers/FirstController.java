8fe05ca841a80745131f84c03c4b0fe3a13fb45d
Karyna Aliushkevich <89156053+KarynaAliushkevich14@users.noreply.github.com>
2022-05-25 13:24:20 +0200

    Spring MVC controllers+model+Thymeleaf

--- spring_mvc_1/src/main/java/springMVC/controllers/FirstController.java
+++ spring_mvc_1/src/main/java/springMVC/controllers/FirstController.java
@@ -1,17 +1,28 @@
 package springMVC.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(HttpServletRequest request, Model model){
+
+        //Parameter ma klucz "name"
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+
+        //System.out.println("Good morning, "+ name+ " "+ surname);
+        model.addAttribute("message", "Good morning, "+ name+ " "+ surname);
         return "first/hello";
     }
+
 
     @GetMapping("/goodbye")
     public String goodbyePage(){
