05b1fb967c6f5d6ea74c7654c1f7b9ae4b5d3dd5
Karyna Aliushkevich <89156053+KarynaAliushkevich14@users.noreply.github.com>
2022-06-16 19:51:27 +0200

    MySQL+JDBC_template finish CRUD list

--- firstCRUD_alishev/src/main/java/myCrud/Config/SpringConfig.java
+++ firstCRUD_alishev/src/main/java/myCrud/Config/SpringConfig.java
@@ -5,12 +5,16 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("myCrud")
@@ -46,5 +50,23 @@ public class SpringConfig implements WebMvcConfigurer {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
         resolver.setTemplateEngine(templateEngine());
         registry.viewResolver(resolver);
+    }
+
+    //Bean JDBC Template
+   @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/person");
+        dataSource.setUsername("root");
+        dataSource.setPassword("25091997Nast");
+
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
     }
 }
--- firstCRUD_alishev/src/main/java/myCrud/DAO/PersonDAO.java
+++ firstCRUD_alishev/src/main/java/myCrud/DAO/PersonDAO.java
@@ -3,6 +3,9 @@ package myCrud.DAO;
 //Zajmije się wyodrębnieniem osoby z listy, znalezieniu osoby według listy, dodawać, uprgade oraz usunięcie
 
 import myCrud.Model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.sql.*;
@@ -11,163 +14,44 @@ import java.util.List;
 
 @Component
 public class PersonDAO {
-    private static int PEOPLE_COUNT;
+    private static JdbcTemplate jdbcTemplate;
 
-
-
-    /*private List<Person> people;
-
-    {
-        people = new ArrayList<Person>();
-
-        people.add (new Person(++PEOPLE_COUNT,"Tom", 24, "tom@mail.ru"));
-        people.add (new Person(++PEOPLE_COUNT, "Bob", 52, "bob@mail.ru"));
-        people.add (new Person(++PEOPLE_COUNT, "Mike", 18, "mike@yahoo.com"));
-        people.add (new Person(++PEOPLE_COUNT, "Katy", 34, "katy@gmail.com"));
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
-    */
-
-    //PODŁĄCZAMU SIĘ DO DB: url DB, imię użytkownika i password
-    private static final String URL = "jdbc:mysql://localhost:3306/person";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "25091997Nast";
-
-    private static Connection connection;
-
-    static {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-
-        try {
-            connection = DriverManager.getConnection(URL,USERNAME, PASSWORD);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
 
     //1 method
     //tylko zwracamy listę people
     public List<Person> index(){
-        //return people;
-
-        List <Person> people = new ArrayList<>();
-        //Объект, который имеет SQL хапрос к базе данных
-        try {
-            Statement statement = connection.createStatement();
-            String SQL = "SELECT * FROM person";
-            ResultSet resultSet = statement.executeQuery(SQL);
-
-            while(resultSet.next()){
-                Person person = new Person();
-
-                person.setId(resultSet.getInt("id"));
-                person.setName(resultSet.getString("name"));
-                person.setAge(resultSet.getInt("age"));
-                person.setEmail(resultSet.getString("email"));
-
-                people.add(person);
-            }
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return people;
+       return jdbcTemplate.query("SELECT * FROM person", new BeanPropertyRowMapper<>(Person.class));
     }
 
     //2 method
     // zwracany osobę według id
-
     public Person showById(int id){
-        /*
-         return people.stream().filter(person->person.getId() == id).findAny().orElse(null);
-         */
-        Person person = null;
-
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM person WHERE id=?");
-            preparedStatement.setInt(1, id);
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            person = new Person();
-
-            person.setId(resultSet.getInt("id"));
-            person.setName(resultSet.getString("name"));
-            person.setEmail(resultSet.getString("email"));
-            person.setAge(resultSet.getInt("age"));
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return person;
+        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny().orElse(null);
     }
 
 
     //3 method
     //dodajemy osobę do naszej listy
     public void save (Person person){
-        //person.setId(++PEOPLE_COUNT);
-        // people.add(person);
-
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO person ( name, age, email) VALUES ( ?, ?, ?)");
-            preparedStatement.setString(1, person.getName());
-            preparedStatement.setInt(2, person.getAge());
-            preparedStatement.setString(3, person.getEmail());
-
-            preparedStatement.executeUpdate();
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        jdbcTemplate.update("INSERT INTO person (name,age,email) VALUES (?, ?, ?)", person.getName(), person.getAge(), person.getEmail());
     }
 
     //3 method
     //update danych za pomocą konkretnrgo id i osoby
-
     public void update(int id, Person updatedPerson){
-        /*
-        Person personToBeUpdated = showById(id);
-
-        personToBeUpdated.setName(updatedPerson.getName());
-        personToBeUpdated.setAge(updatedPerson.getAge());
-        personToBeUpdated.setEmail(updatedPerson.getEmail());
-        */
-        try {
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("UPDATE Person SET name=?, age=?, email=? WHERE id=?");
-
-            preparedStatement.setInt(1, id);//WTF
-
-            preparedStatement.setString(1, updatedPerson.getName());
-            preparedStatement.setInt(2, updatedPerson.getAge());
-            preparedStatement.setString(3, updatedPerson.getEmail());
-            preparedStatement.setInt(4, id);
-
-            preparedStatement.executeUpdate();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+        jdbcTemplate.update("UPDATE person SET name=?, age=?, email=? WHERE id =?", updatedPerson.getName(), updatedPerson.getAge(), updatedPerson.getEmail(), id);
     }
 
 
     //4 method
     //usuwanie danych za pomocą konkretnrgo id
-    public void delete(int id){
-        //people.removeIf(p -> p.getId() == id);
-        PreparedStatement preparedStatement =
-                null;
-        try {
-            preparedStatement = connection.prepareStatement("DELETE FROM Person WHERE id=?");
-
-            preparedStatement.setInt(1, id); //WTF
-
-            preparedStatement.executeUpdate();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+    public void delete(int id) {
+        jdbcTemplate.update("DELETE FROM person WHERE id=?", id);
     }
 
 
